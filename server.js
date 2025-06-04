46b990ea3e33cf42cb97475cb0bd1a949fe91a09
Beulah46 <zunglianthangbeulah@gmail.com>
2025-06-04 14:19:18 +0930

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,32 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.post('/create-subscription', async (req, res) => {
-  const { email } = req.body;
+  const { email, name, phone, address } = req.body;
+
+  console.log("New Donor Info:");
+  console.log("Name:", name);
+  console.log("Email:", email);
+  console.log("Phone:", phone);
+  console.log("Address:", address);
+
+  // Here you could add code to save to a database or email service
+
+  const customer = await stripe.customers.create({
+    email,
+    name,
+    phone,
+    address: {
+      line1: address
+    }
+  });
+
+  const setupIntent = await stripe.setupIntents.create({
+    customer: customer.id,
+  });
+
+  res.send({ clientSecret: setupIntent.client_secret });
+});
+
   const customer = await stripe.customers.create({ email });
   const setupIntent = await stripe.setupIntents.create({ customer: customer.id });
   res.send({ clientSecret: setupIntent.client_secret });
